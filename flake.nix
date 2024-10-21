ec268cce1140c7423138b3db93a0859ec685f354
Alex Jackson <contact@ajaxbits.com>
2024-10-21 11:08:15 -0500

    chore: update

--- flake.nix
+++ flake.nix
@@ -3,35 +3,43 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*.tar.gz";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-    version = builtins.substring 0 8 lastModifiedDate;
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
 
-    supportedSystems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-  in {
-    packages = forAllSystems (system: let
-      pkgs = nixpkgsFor.${system};
-    in {
-      caddy = pkgs.buildGoModule {
-        pname = "caddy";
-        inherit version;
-        src = ./caddy-src;
-        runVend = true;
-        vendorHash = "sha256-fgr64f1wOreFrMTAMnYTkaqAfrcG0tme4J3YVawoks0=";
-      };
-    });
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          caddy = pkgs.buildGo123Module {
+            pname = "caddy";
+            inherit version;
+            src = ./caddy-src;
+            runVend = true;
+            vendorHash = "sha256-fgr64f1wOreFrMTAMnYTkaqAfrcG0tme4J3YVawoks0=";
+            # vendorSha256 = pkgs.lib.fakeSha256;
+          };
+        }
+      );
 
-    defaultPackage = forAllSystems (system: self.packages.${system}.caddy);
-  };
+      defaultPackage = forAllSystems (system: self.packages.${system}.caddy);
+    };
 }

67985993e4d824cd4705446452354c9605d3744f
Samuel Corsi-House <sam@chouse.dev>
2024-11-03 13:24:21 -0500

    fix: warning

--- flake.nix
+++ flake.nix
@@ -35,6 +35,9 @@
             src = ./caddy-src;
             runVend = true;
             vendorHash = "sha256-dEuxEG6mW2V7iuSXvziR82bmF+Hwe6ePCfdNj5t3t4c=";
+            meta = {
+              mainProgram = "caddy";
+            };
           };
         }
       );
